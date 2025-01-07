3f085710527fac7243c64896b3aee84893caea35
gabrielebuoso <buosogabriele@gmail.com>
2025-01-07 16:34:42 +0100

    structure layout setup

---
+++ frontend/src/ui/LayoutNavbar.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+function LayoutNavbar({children}) {
+    return (
+        <div className="navbar__container">
+            {children}
+        </div>
+    )
+}
+
+export default LayoutNavbar
