88429bcafdae35b5c2756186d20d1783c543e9c0
gabrielebuoso <buosogabriele@gmail.com>
2025-01-07 16:34:36 +0100

    structure layout setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <div>
+            This is the first demo of Navbar!
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/ui/Layout.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+function Layout({children}) {
+    return (
+        <div className="page__container">
+            {children}
+        </div>
+    )
+}
+
+export default Layout
