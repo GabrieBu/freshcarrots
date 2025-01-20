429746a4608af9ccd8cf82510d6936c6fec44a4a
gabrielebuoso <buosogabriele@gmail.com>
2025-01-20 17:47:04 +0100

    feature: test axios query to main server

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,4 +1,5 @@
-import {lazy} from "react";
+import {lazy, useEffect} from "react";
+import axios from "axios";
 
 const Layout = lazy(() => import("../ui/Layout"));
 const LayoutNavbar = lazy(() => import("../ui/LayoutNavbar"));
@@ -7,6 +8,22 @@ const Footer = lazy(() => import("../components/Footer"));
 const Navbar = lazy(() => import("../components/Navbar"));
 
 function Community() {
+    const first_url = "http://localhost:3000/";
+
+    useEffect(() => {
+        function sendAxiosQuery(url) {
+            axios.get(url)
+                .then (function (dataR) {
+                    console.log("Received first data: " + dataR.data);
+                })
+                .catch(function (error) {
+                    console.error("Error:", error.message);
+                });
+        }
+        console.log(`Query sent to ${first_url} waiting`);
+        sendAxiosQuery(first_url);
+    }, []);
+
     return (
         <Layout>
             <LayoutNavbar>
