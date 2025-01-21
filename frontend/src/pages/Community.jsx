468e9d8107ba74c063e4f6909c409b568ff228cf
gabrielebuoso <buosogabriele@gmail.com>
2025-01-21 11:31:43 +0100

    fixing cors

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,4 +1,4 @@
-import {lazy, useEffect} from "react";
+import { lazy, useEffect } from "react";
 import axios from "axios";
 
 const Layout = lazy(() => import("../ui/Layout"));
@@ -8,32 +8,33 @@ const Footer = lazy(() => import("../components/Footer"));
 const Navbar = lazy(() => import("../components/Navbar"));
 
 function Community() {
-    const first_url = "http://localhost:3000/";
+  const first_url = "http://localhost:3000/";
 
-    useEffect(() => {
-        function sendAxiosQuery(url) {
-            axios.get(url)
-                .then (function (dataR) {
-                    console.log("Received first data: " + dataR.data);
-                })
-                .catch(function (error) {
-                    console.error("Error:", error.message);
-                });
-        }
-        console.log(`Query sent to ${first_url} waiting`);
-        sendAxiosQuery(first_url);
-    }, []);
+  useEffect(() => {
+    function sendAxiosQuery(url) {
+      axios
+        .get(url, { withCredentials: true })
+        .then(function (dataR) {
+          console.log("Received first data: " + dataR.data);
+        })
+        .catch(function (error) {
+          console.error("Error:", error.message);
+        });
+    }
+    console.log(`Query sent to ${first_url} waiting`);
+    sendAxiosQuery(first_url);
+  }, []);
 
-    return (
-        <Layout>
-            <LayoutNavbar>
-                <Navbar />
-            </LayoutNavbar>
-            <LayoutContent>
-                <p>Content</p>
-            </LayoutContent>
-            <Footer />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <LayoutNavbar>
+        <Navbar />
+      </LayoutNavbar>
+      <LayoutContent>
+        <p>Content</p>
+      </LayoutContent>
+      <Footer />
+    </Layout>
+  );
 }
-export default Community
+export default Community;
