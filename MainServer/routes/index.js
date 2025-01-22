2ed1d1ced8e3cb3452e1ec65063742c175fcf012
gabrielebuoso <buosogabriele@gmail.com>
2025-01-22 14:59:33 +0100

    feature: routes actors, movies, reviews added

--- MainServer/routes/index.js
+++ MainServer/routes/index.js
@@ -3,41 +3,37 @@ var router = express.Router();
 const axios = require('axios')
 
 /* GET movies */
-router.get('/movies', async function(req, res, next) {
+router.get('/getMovies', async function(req, res, next) {
   try {
     const response = await axios.get(
-        'http://localhost:3001/get_all_movies');
+        'http://localhost:3002/getMovies');
     res.json(response.data);
   } catch (error) {
-    res.status(500).send('Error occured: get_movies ' + error.message);
+    res.status(500).send('Error occured: getMovies ' + error.message);
   }
 });
 
+/* GET actors */
+router.get('/getActors', async function(req, res, next) {
+  try {
+    const response = await axios.get(
+        'http://localhost:3002/getActors');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error occured: getActors ' + error.message);
+  }
+});
 
-/* GET reviews */
-router.get('/get_review/:id', async function(req, res, next) {
+/* GET reviews. */
+router.get('/getReviews', async function(req, res, next) {
   const { id } = req.params;
   try {
     const response = await axios.get(
-        `http://localhost:3001/get_review_movie/${id}`);
+        'http://localhost:3001/getReviews');
     res.json(response.data);
   } catch (error) {
-    res.status(500).send('Error occured: get_review_movie ' + error.message);
+    res.status(500).send('Error occured: getReviews ' + error.message);
   }
 });
-
-
-/* GET actors. */
-router.get('/actor/:id', async function(req, res, next) {
-  const { id } = req.params;
-  try {
-    const response = await axios.get(
-        'http://localhost:3002/get_actors_movie/${id}');
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error occured: get_actor_movie ' + error.message);
-  }
-});
-
 
 module.exports = router;
