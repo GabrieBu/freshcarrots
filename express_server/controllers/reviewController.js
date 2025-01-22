bdc7a8340090a957c10f8c181e17a0f927c9dcb3
gabrielebuoso <buosogabriele@gmail.com>
2025-01-22 20:43:57 +0100

    feature: implement pagination getReviews

--- express_server/controllers/reviewController.js
+++ express_server/controllers/reviewController.js
@@ -1,8 +1,17 @@
 import Review from "../models/Review.js";
 
+/*
+* @TODO add filters option
+* */
+
 export const getReviews = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const toSkip = (page - 1) * 10;
     try {
-        const review = await Review.find(undefined, undefined, undefined).limit(100); //limit to 100 otherwise it would be a BIG query
+        const review = await Review.find(undefined, undefined, undefined)
+            .sort({ review_date: -1 })
+            .skip(toSkip)  // skip previous pages
+            .limit(10); // one page = 10 reviews
         console.log("Review returned: " + JSON.stringify(review));
         res.json(review);
     } catch (error) {
