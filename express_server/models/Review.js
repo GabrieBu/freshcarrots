bfef45bfda77bc5035c515a521a3c591be9e71b0
gabrielebuoso <buosogabriele@gmail.com>
2025-01-22 17:08:41 +0100

    feature: Review Schema

--- express_server/models/Review.js
+++ express_server/models/Review.js
@@ -2,12 +2,15 @@ import mongoose, {Schema} from "mongoose";
 
 var ReviewSchema = new Schema(
     {
-        /*id: {type: Number},
-        name: {type: String},
-        date: {type: Number},
-        tagline: {type: String},
-        description: {type: String},
-        minute: {type: Number},*/
+        id: {type: Schema.Types.ObjectId, ref: 'Review'},
+        movie_title: {type: String},
+        critic_name: {type: String},
+        top_critic: {type: Boolean},
+        publisher_name: {type: String},
+        review_type: {type: String},
+        review_date: {type: Date},
+        review_content: {type: String},
+        link: {type: String},
     }
 );
 
