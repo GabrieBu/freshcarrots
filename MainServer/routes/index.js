a9676ffa17b8e7d5af5a0809c02aeb80cd4d25a1
gabrielebuoso <buosogabriele@gmail.com>
2025-01-24 15:16:05 +0100

    feature: filters params to express_server

--- MainServer/routes/index.js
+++ MainServer/routes/index.js
@@ -2,39 +2,20 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios')
 
-/* GET movies */
-router.get('/getMovies', async function(req, res, next) {
-  try {
-    const response = await axios.get(
-        'http://localhost:3002/getMovies',{params: req.query});
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error occured: getMovies ' + error.message);
-  }
-});
-
-/* GET actors */
-router.get('/getActors', async function(req, res, next) {
-  try {
-    const response = await axios.get(
-        'http://localhost:3002/getActors');
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error occured: getActors ' + error.message);
-  }
-});
-
 /* GET reviews. */
 router.get('/getReviews', async function(req, res, next) {
   try {
-    const response = await axios.get(
-        'http://localhost:3001/getReviews', {params: req.query});
+    const response = await axios.get('http://localhost:3001/getReviews', {
+      params: req.query // Pass all query parameters dynamically
+    });
+
     res.json(response.data);
   } catch (error) {
     res.status(500).send('Error occured: getReviews ' + error.message);
   }
 });
 
+/* @TODO impelement it frontend side */
 /* GET reviews by movie. */
 router.get('/getReviewsByMovie', async function(req, res, next) {
   const { movie_title } = req.query;
