8d2f11014ba0cee933eec63f74c768055a15b862
AndreaDriza <andreadrizao@gmail.com>
2025-01-24 11:09:45 +0100

    RouteReviewByMovie

--- MainServer/routes/index.js
+++ MainServer/routes/index.js
@@ -6,7 +6,7 @@ const axios = require('axios')
 router.get('/getMovies', async function(req, res, next) {
   try {
     const response = await axios.get(
-        'http://localhost:3002/getMovies');
+        'http://localhost:3002/getMovies',{params: req.query});
     res.json(response.data);
   } catch (error) {
     res.status(500).send('Error occured: getMovies ' + error.message);
@@ -34,5 +34,20 @@ router.get('/getReviews', async function(req, res, next) {
     res.status(500).send('Error occured: getReviews ' + error.message);
   }
 });
+
+/* GET reviews by movie. */
+router.get('/getReviewsByMovie', async function(req, res, next) {
+  const { movie_title } = req.query;
+  try {
+    const response = await axios.get(
+        'http://localhost:3001/getReviewsByMovie', { params: { movie_title } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error occurred: getReviewsByMovie ' + error.message);
+  }
+});
+
+
 
 module.exports = router;
