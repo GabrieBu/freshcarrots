ddbc81f239d8dd2ec8ce0a931a721c5fed47393b
gabrielebuoso <buosogabriele@gmail.com>
2025-01-27 22:55:12 +0100

    feature: getMessages

--- MainServer/routes/index.js
+++ MainServer/routes/index.js
@@ -45,4 +45,15 @@ router.get('/getDiscussions', async function(req, res, next) {
   }
 });
 
+router.get('/getMessages', async function(req, res, next) {
+  try {
+    const response = await axios.get('http://localhost:3001/getMessages', {
+      params: req.query // pass all query parameters to the express_server
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error occured: getReviews ' + error.message);
+  }
+});
+
 module.exports = router;
