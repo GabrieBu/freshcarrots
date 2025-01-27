8d531fe2fcba62cc1c16b521ef3d3907483529f3
gabrielebuoso <buosogabriele@gmail.com>
2025-01-27 23:09:17 +0100

    feature: getMessages and newMessage routes

--- express_server/routes/index.js
+++ express_server/routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 var router = express.Router();
 import {getReviews} from "../controllers/reviewController.js";
-import {newDiscussion, getDiscussions} from "../controllers/discussionController.js";
+import {newDiscussion, getDiscussions, getMessages, newMessage} from "../controllers/discussionController.js";
 
 /* GET home page. UNUSED SO FAR */
 router.get('/', function(req, res, next) {
@@ -14,6 +14,10 @@ router.get('/getReviews', getReviews)
 router.post('/newDiscussion', newDiscussion)
 
 router.get('/getDiscussions', getDiscussions)
+
+router.get('/getMessages', getMessages)
+
+router.post("/newMessage", newMessage);
 
 
 
