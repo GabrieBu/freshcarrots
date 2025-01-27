5ba85e1ae082556f1cd804e26b9bccda0348b019
gabrielebuoso <buosogabriele@gmail.com>
2025-01-28 00:06:42 +0100

    setup

--- MainServer/routes/index.js
+++ MainServer/routes/index.js
@@ -58,9 +58,7 @@ router.get('/getMessages', async function(req, res, next) {
 
 router.post('/newMessage', async function(req, res, next) {
   try {
-    const response = await axios.post('http://localhost:3001/newMessage', {
-      params: req.query // pass all query parameters to the express_server
-    });
+    const response = await axios.post('http://localhost:3001/newMessage', req.body);
     res.json(response.data);
   } catch (error) {
     res.status(500).send('Error occured: newMessage ' + error.message);
