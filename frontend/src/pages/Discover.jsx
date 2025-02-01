54ac6ffd5061f4654fd309d66925de5794486c71
gabrielebuoso <buosogabriele@gmail.com>
2025-02-01 17:23:23 +0100

    feature: working on all movies page, filters logic and reusable component

--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -6,22 +6,60 @@ import Navbar from "../components/Navbar.jsx";
 import {useState} from "react";
 import MovieCard from "../components/MovieCard.jsx";
 import useMovies from "../hooks/useMovies.js";
+import DropdownFilter from "../ui/DropdownFilter.jsx";
 
-const genres = ["Action", "Comedy", "Drama"]
+const filters = [{
+        typeFilter: "title",
+        options: [{
+            name: "aToZ",
+            label: "From A to Z"
+        }, {
+            name: "zToA",
+            label: "From Z to A"
+        }]
+    },
+    {
+        typeFilter: "date",
+        options: [{
+            name: "newestToOldest",
+            label: "From newest to oldest"
+        }, {
+            name: "oldestToNewest",
+            label: "From oldest to newest"
+        }]
+    },
+    {
+        typeFilter: "rating",
+        options: [{
+            name: "zeroToOne",
+            label: "[0-1]"
+        }, {
+            name: "oneToTwo",
+            label: "[1-2]"
+        },
+        {
+            name: "twoToThree",
+            label: "[2-3]"
+        },
+        {
+            name: "threeToFour",
+            label: "[3-4]"
+        },
+        {
+            name: "fourToFive",
+            label: "[4-5]"
+        }]
+    }
+]
 
 function Discover() {
     const [pageNumber, setPageNumber] = useState(1);
     const [selectedFilters, setSelectedFilters] = useState([]);
-    const [isOpenOrder, setIsOpenOrder] = useState(false);
-    const [isOpenGenre, setIsOpenGenre] = useState(false);
 
-    const {movies, loading, error} = useMovies();
+    const {movies} = useMovies();
 
-    function handleFilterChange(event) {
-        const { value, checked } = event.target;
-        setSelectedFilters((prev) =>
-            checked ? [...prev, value] : prev.filter((filter) => filter !== value)
-        );
+    function handleResetFilter() {
+        setSelectedFilters([]);
         setPageNumber(1);
     }
 
@@ -34,145 +72,9 @@ function Discover() {
             <h1>All movies: </h1>
             <div className="bg-light p-3 mb-4 shadow-sm rounded" style={{border: "1px solid #ccc"}}>
                 <div className="row gy-2">
+                    {filters.map((filter, index) => <DropdownFilter key={index} filterObj={filter} onSelectedFilters={selectedFilters} onSetSelectedFilters={setSelectedFilters} onSetPageNumber={setPageNumber}/>)}
                     <div className="col-md-2">
-                        <label htmlFor="orderBy" className="form-label">Order By:</label>
-                        <div className="dropdown">
-                            <button
-                                id="orderBy"
-                                className="btn btn-outline-secondary dropdown-toggle"
-                                type="button"
-                                onClick={() => setIsOpenOrder(!isOpenOrder)}
-                            >
-                                Select
-                            </button>
-                            <div className={`dropdown-menu p-3 ${isOpenOrder ? "show" : ""}`}
-                                 style={{minWidth: "200px"}}>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="oldestToNewest"
-                                        className="form-check-input"
-                                        value="oldestToNewest"
-                                        checked={selectedFilters.includes("oldestToNewest")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="oldestToNewest" className="form-check-label">
-                                        From oldest to newest
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="newestToOldest"
-                                        className="form-check-input"
-                                        value="newestToOldest"
-                                        checked={selectedFilters.includes("newestToOldest")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="newestToOldest" className="form-check-label">
-                                        From newest to oldest
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="aToZ"
-                                        className="form-check-input"
-                                        value="aToZ"
-                                        checked={selectedFilters.includes("aToZ")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="aToZ" className="form-check-label">
-                                        From A-Z
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="zToA"
-                                        className="form-check-input"
-                                        value="zToA"
-                                        checked={selectedFilters.includes("zToA")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="zToA" className="form-check-label">
-                                        From Z-A
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-2">
-                        <label htmlFor="orderByGenre" className="form-label">Genre:</label>
-                        <div className="dropdown">
-                            <button
-                                id="orderByGenre"
-                                className="btn btn-outline-secondary dropdown-toggle"
-                                type="button"
-                                onClick={() => setIsOpenOrder(!isOpenOrder)}
-                            >
-                                Select
-                            </button>
-                            <div className={`dropdown-menu p-3 ${isOpenOrder ? "show" : ""}`}
-                                 style={{minWidth: "200px"}}>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="oldestToNewest"
-                                        className="form-check-input"
-                                        value="allGenres"
-                                        checked={selectedFilters.includes("oldestToNewest")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="oldestToNewest" className="form-check-label">
-                                        From oldest to newest
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="newestToOldest"
-                                        className="form-check-input"
-                                        value="newestToOldest"
-                                        checked={selectedFilters.includes("newestToOldest")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="newestToOldest" className="form-check-label">
-                                        From newest to oldest
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="aToZ"
-                                        className="form-check-input"
-                                        value="aToZ"
-                                        checked={selectedFilters.includes("aToZ")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="aToZ" className="form-check-label">
-                                        From A-Z
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input
-                                        type="checkbox"
-                                        id="zToA"
-                                        className="form-check-input"
-                                        value="zToA"
-                                        checked={selectedFilters.includes("zToA")}
-                                        onChange={handleFilterChange}
-                                    />
-                                    <label htmlFor="zToA" className="form-check-label">
-                                        From Z-A
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-2">
-                        <button className="btn btn-outline-secondary">
+                        <button className="btn btn-outline-secondary" onClick={handleResetFilter}>
                             Reset Filters
                         </button>
                     </div>
