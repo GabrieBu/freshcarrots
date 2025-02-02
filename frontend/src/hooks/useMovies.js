883b5f31243818abddf5e9cec8a7ef7b94644007
gabrielebuoso <buosogabriele@gmail.com>
2025-02-02 17:39:38 +0100

    feature: added query to getFilteredMovies

--- frontend/src/hooks/useMovies.js
+++ frontend/src/hooks/useMovies.js
@@ -1,21 +1,36 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-export default function useMovies(pageNumber) {
+export default function useMovies(pageNumber, selectedFilters) {
     const [movie, setMovie] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
     useEffect(() => {
         setLoading(true);
+        let  filter = {};
+
+        selectedFilters.forEach(value => {
+            if (value === "ascName") filter.orderByName = "asc";
+            if (value === "descName") filter.orderByName = "desc";
+
+            if (value === "ascDate") filter.orderByDate = "asc";
+            if (value === "descDate") filter.orderByDate = "desc";
+
+            if (["zeroToOne", "oneToTwo", "twoToThree", "threeToFour", "fourToFive"].includes(value)) {
+                filter.byRating = value;
+            }
+            if (!filter.byRating && !filter.orderByName && !filter.orderByDate) {
+                filter.genre = value;
+            }
+        });
+        filter = {...filter, page: pageNumber};
+        console.log(filter)
         setError(false);
         axios({
             method: "GET",
             url: `http://localhost:3000/getFilteredMovies`,
-            params: {
-                page: pageNumber,
-                /*TODO add others params*/
-            }
+            params: filter
         })
             .then((res) => {
                 setMovie(res.data);
@@ -26,7 +41,7 @@ export default function useMovies(pageNumber) {
                 setError(true);
                 setLoading(false);
             });
-    }, [pageNumber]);
+    }, [pageNumber, selectedFilters]);
 
-    return { movie, loading, error };
+    return { movie, loadingMovies: loading, errorMovies: error };
 }
