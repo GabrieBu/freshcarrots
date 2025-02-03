522dec988bebeaebe3e119154ef3f1c8615c1a7e
AndreaDriza <andreadrizao@gmail.com>
2025-02-03 19:00:55 +0100

    Swagger

--- spring_boot_server/spring_boot_server/src/main/java/com/example/spring_boot_server/crew/CrewController.java
+++ spring_boot_server/spring_boot_server/src/main/java/com/example/spring_boot_server/crew/CrewController.java
@@ -3,11 +3,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/crew")
+@Tag(name = "Crew", description = "Operations related to Crew")
 public class CrewController {
     private final CrewService crewService;
 
@@ -17,7 +21,8 @@ public class CrewController {
     }
 
     @GetMapping("/findByMovie")
-    public ResponseEntity<List<Crew>>findCrewByMovie(@RequestParam Integer id) {
+    @Operation(summary = "find crew by movie id", description = "Get all crew members of a movie")
+    public ResponseEntity<List<Crew>>findCrewByMovie(@Parameter(description = "Movie id to search") @RequestParam Integer id) {
         List<Crew> crew = crewService.findCrewByMovie(id);
         if (crew.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
--- spring_boot_server/spring_boot_server/src/main/java/com/example/spring_boot_server/genres/GenresController.java
+++ spring_boot_server/spring_boot_server/src/main/java/com/example/spring_boot_server/genres/GenresController.java
@@ -5,14 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/genres")
+@Tag(name = "Genres", description = "Operations related to Genres")
 public class GenresController {
     @Autowired
     private GenresService genresService;
 
     @GetMapping("/getGenres")
+    @Operation(summary = "Get all movies genres name", description = "Get all genres")
     public ResponseEntity<List<GenreNameDTO>>findGenres() {
         List<GenreNameDTO> genres = genresService.findDistinctGenres();
         if (genres.isEmpty()) {
