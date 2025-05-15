1e0703f1fc8cc512ded92eb9b4193a08a2f4904b
gabrielebuoso <buosogabriele@gmail.com>
2025-05-15 16:37:28 +0200

    feature: new simplified logic for discussions

---
+++ frontend/src/components/UserModal.jsx
@@ -0,0 +1,62 @@
+import {useEffect, useState} from "react";
+
+function UserModal({ username, onSetUsername }) {
+    const [errorModal, setErrorModal] = useState(false);
+
+    useEffect(() => {
+        /* initialize username retrieving from local storage */
+        const user = JSON.parse(localStorage.getItem("username")) || "";
+        if (user) {
+            onSetUsername(user);
+        } else {
+            //if not present ask the user to insert it
+            const modalElement = document.getElementById("modalUsername");
+            const modal = new window.bootstrap.Modal(modalElement);
+            modal.show(); //show the bootstrap modal
+            return;
+        }
+        //retrieve past followed discussion from the localstorage, if present
+    }, []);
+
+    // submiut username in the modal, store in useState
+    function handleSubmitModal() {
+        if(username!== "") {
+            localStorage.setItem("username", JSON.stringify(username));
+            const modalElement = document.getElementById("modalUsername");
+            const modal = window.bootstrap.Modal.getInstance(modalElement)
+            modal.hide();
+
+            document.querySelectorAll(".modal-backdrop").forEach((el) => el.remove());
+        }
+        else{
+            setErrorModal(true);
+        }
+    }
+
+    return (<>
+        <div id="modalUsername" className="modal fade" tabIndex="-1">
+            <div className="modal-dialog modal-sm">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Enter a username to discuss among members.</h5>
+                    </div>
+                    <div className="modal-body">
+                        {errorModal && <h6 className="text-danger">Username is required</h6>}
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Type..."
+                            value={username}
+                            onChange={(e) => onSetUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-primary" onClick={handleSubmitModal}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>)
+}
+
+export default UserModal
--- frontend/src/hooks/useDiscussions.js
+++ frontend/src/hooks/useDiscussions.js
@@ -1,50 +1,34 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import movie from "../pages/Movie.jsx";
 
-export default function useDiscussions(pageNumber, movieFilter, sortByDate) {
+export default function useDiscussions(movieQuery, sortByDate) {
     const [discussions, setDiscussions] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
-    const [hasMore, setHasMore] = useState(true);
 
     useEffect(() => {
         setLoading(true);
         setError(false);
 
-        const controller = new AbortController();
-
         axios({
             method: "GET",
             url: `http://localhost:3000/getDiscussions`,
             params: {
-                page: pageNumber,
-                movieFilter,
+                movieQuery,
                 sortByDate
-            },
-            signal: controller.signal
+            }
         })
             .then((res) => {
-                const newDiscussions = res.data;
-
-                setDiscussions((prev) =>
-                    pageNumber === 1
-                        ? newDiscussions  // if it's the first page, reset discussions
-                        : [...prev, ...newDiscussions]  // else  append new discussions
-                );
-
-                setHasMore(newDiscussions?.length > 0); //update hasMore:
-
+                setDiscussions(res.data); // else  append new discussions
                 setLoading(false);
             })
             .catch((err) => {
-                if (axios.isCancel(err)) return;
                 console.error(err);
                 setError(true);
                 setLoading(false);
             });
+    }, [movieQuery, sortByDate]);
 
-        return () => controller.abort();
-    }, [pageNumber, movieFilter, sortByDate]);
-
-    return { discussions, setDiscussions, loading, error, hasMore };
+    return { discussions, loading, error };
 }
